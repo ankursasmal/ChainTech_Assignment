50960a77da8e25a14449247ade8beb515dd28c34
ANKUR SASMAL <ankursasmal@ANKURs-MacBook-Air.local>
2024-03-28 18:51:08 +0530

    complete

--- All.js
+++ All.js
@@ -1,13 +1,9 @@
 let buttonaction=document.querySelectorAll('.buttonaction');
 let state=true;
 
-// buttonaction.forEach(e=>{
-//     e.addEventListener('click',()=>{
-//         state===false?state=true:state=false;
-//        state? e.innerHTML=`<a  style="padding-right: 15px !important;padding-left: 7px !important;color:red">Explore</a>`:e.innerHTML=`<a  style="padding-right: 15px !important;padding-left: 7px !important;color:red">Explore</a>`;
-//     })
-// })
-// e.addEventListener('mouseover',()=>{
-//     state===false?state=true:state=false;
-//    !state? e.innerHTML=`<a  style="padding-right: 15px !important;padding-left: 7px !important;color:red">Explore</a>`:e.innerHTML=`<a  style="padding-right: 15px !important;padding-left: 7px !important;color:red">Explore</a>`;
-// })
+buttonaction.forEach(e=>{
+    e.addEventListener('click',()=>{
+        state===false?state=true:state=false;
+       state? e.innerHTML=`<a  style="padding-right: 15px !important;padding-left: 7px !important;color:red">Explore</a>`:e.innerHTML=`<a  style="padding-right: 15px !important;padding-left: 7px !important;color:red">Explore</a>`;
+    })
+}) 
