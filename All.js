e4d093834d0a51d3f4aec9b6c74c5fcad91858f1
ANKUR SASMAL <ankursasmal@ANKURs-MacBook-Air.local>
2024-03-28 18:47:57 +0530

    complete

---
+++ All.js
@@ -0,0 +1,13 @@
+let buttonaction=document.querySelectorAll('.buttonaction');
+let state=true;
+
+// buttonaction.forEach(e=>{
+//     e.addEventListener('click',()=>{
+//         state===false?state=true:state=false;
+//        state? e.innerHTML=`<a  style="padding-right: 15px !important;padding-left: 7px !important;color:red">Explore</a>`:e.innerHTML=`<a  style="padding-right: 15px !important;padding-left: 7px !important;color:red">Explore</a>`;
+//     })
+// })
+// e.addEventListener('mouseover',()=>{
+//     state===false?state=true:state=false;
+//    !state? e.innerHTML=`<a  style="padding-right: 15px !important;padding-left: 7px !important;color:red">Explore</a>`:e.innerHTML=`<a  style="padding-right: 15px !important;padding-left: 7px !important;color:red">Explore</a>`;
+// })
